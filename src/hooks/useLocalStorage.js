aaa3dabdaf8acc3e074d5c13eb79e5d4bef7dba3
MadisonViaLux <madison.s.mckown@gmail.com>
2019-07-18 08:09:08 -0700

    useLocalStorage hook built v2

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -3,7 +3,9 @@ import {useState} from 'react'
 
 
 export const useLocalStorage = (key, initialValue) => {
+    
     const [storedValue, setStoredValue] = useState(() => {
+
       const item = window.localStorage.getItem(key);
 
       const setValue = value => {
